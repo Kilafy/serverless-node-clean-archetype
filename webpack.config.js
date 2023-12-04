d3dc4c066cffe14b52db09bd49252326380d1c36
Darwin Grisales <darwingrisalesg@gmail.com>
2023-12-04 05:45:06 -0500

    :art: [chore][webpack]: update format code of webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-const path = require('path')
-const slsw = require('serverless-webpack')
+/* eslint-disable no-return-assign */
+const path = require('path');
+const slsw = require('serverless-webpack');
 
 const entries = {};
 
 Object.keys(slsw.lib.entries).forEach(
-    (key) => (entries[key] = [".source-map-install.js", slsw.lib.entries[key]])
-)
+	key => (entries[key] = ['.source-map-install.js', slsw.lib.entries[key]]),
+);
 
 module.exports = {
-    mode: slsw.lib.webpack.isLocal ? "development" : "production",
-    entry: slsw.lib.entries,
-    devtool: "source-map",
-    resolve: {
-        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
-    },
-    output: {
-        libraryTarget: "commonjs",
-        path: path.join(__dirname, ".webpack"),
-        filename: "[name].js",
-    },
-    target: "node",
-    module: {
-        rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            {
-                test: /\.tsx?%/,
-                loader: "ts-loader"
-            }
-        ]
-    }
-}
+	mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+	entry: slsw.lib.entries,
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+	},
+	output: {
+		libraryTarget: 'commonjs',
+		path: path.join(__dirname, '.webpack'),
+		filename: '[name].js',
+	},
+	target: 'node',
+	module: {
+		rules: [
+			// All files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+			{
+				test: /\.tsx?%/,
+				loader: 'ts-loader',
+			},
+		],
+	},
+};
