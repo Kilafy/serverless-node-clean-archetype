6771cdbaee8f53571b8ac45e2fe72c3e32fa4e5d
Darwin Grisales <darwingrisalesg@gmail.com>
2024-01-10 06:25:31 -0500

    style: format code

--- src/infrastructure/driving/apigateway/controllerApigateway.ts
+++ src/infrastructure/driving/apigateway/controllerApigateway.ts
@@ -1,35 +1,36 @@
-import {type APIGatewayEvent} from 'aws-lambda';
-import {zodValidator} from '../../../utils/zodValidator';
-import type z from 'zod';
-import {type ApiResponse, getHttpResponse} from '../../../utils/apiResponse';
-import {decodeJwt} from '../../../utils/decodeJws';
-import {type BasicUseCase} from '../../../app/usecases/basicUseCase';
+import { type APIGatewayEvent } from "aws-lambda";
+import { zodValidator } from "../../../utils/zodValidator";
+import type z from "zod";
+import { type ApiResponse, getHttpResponse } from "../../../utils/apiResponse";
+import { decodeJwt } from "../../../utils/decodeJws";
+import { type BasicUseCase } from "../../../app/usecases/basicUseCase";
 
 type ControllerApigatewayHttp = (
-	serviceUseCase: BasicUseCase,
-	zodSchema: z.Schema
+  serviceUseCase: BasicUseCase,
+  zodSchema: z.Schema,
 ) => (event: APIGatewayEvent, context: unknown) => Promise<ApiResponse>;
 
-export const controllerApigatewayHttp: ControllerApigatewayHttp
-= (serviceUseCase: BasicUseCase, zodSchema: z.Schema) => async (event: APIGatewayEvent, context: unknown) => {
-	try {
-		console.log(context);
-		const body = JSON.parse(event.body ?? '{}') as Record<string, unknown>;
+export const controllerApigatewayHttp: ControllerApigatewayHttp =
+  (serviceUseCase: BasicUseCase, zodSchema: z.Schema) =>
+  async (event: APIGatewayEvent, context: unknown) => {
+    try {
+      console.log(context);
+      const body = JSON.parse(event.body ?? "{}") as Record<string, unknown>;
 
-		const validateResult = zodValidator(
-			zodSchema,
-			body,
-		);
+      const validateResult = zodValidator(zodSchema, body);
 
-		if (!validateResult.success) {
-			return getHttpResponse.badRequest(validateResult.message, validateResult.errors);
-		}
+      if (!validateResult.success) {
+        return getHttpResponse.badRequest(
+          validateResult.message,
+          validateResult.errors,
+        );
+      }
 
-		const jwtPayload = decodeJwt(String(event.headers.Authorization));
-		const result = await serviceUseCase.run(body, jwtPayload?.aud);
+      const jwtPayload = decodeJwt(String(event.headers.Authorization));
+      const result = await serviceUseCase.run(body, jwtPayload?.aud);
 
-		return result;
-	} catch (error) {
-		return getHttpResponse.serviceUnavailable(error as string);
-	}
-};
+      return result;
+    } catch (error) {
+      return getHttpResponse.serviceUnavailable(error as string);
+    }
+  };
