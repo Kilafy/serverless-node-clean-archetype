077e81adc0a4f349eb0d6c356c6f4a22feec049d
camilo alvarez <andres.alvarez@bam.com.gt>
2024-08-02 18:18:55 -0500

    [feature] se agrega pre-push hook

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,20 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js'],
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': 'ts-jest',
   },
   clearMocks: true,
   collectCoverage: true,
   coverageProvider: 'v8',
   testMatch: ['**/test/**/*.test.ts'],
   coverageDirectory: './coverage',
-  collectCoverageFrom: ['src/**/*.ts']
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
